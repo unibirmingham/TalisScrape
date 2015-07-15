28ef6ee927fc86f3238aee469b1548157aaee676
Chris <chris.withers@gmail.com>
2015-07-15 23:38:29 +0100

    Made some optimizations of book scraping, added Exporter functionality and started to create function to Parse IEnumerable<ReadingList> into correct OAI-PMH xml format. Added a basic wpf app to test async functionality of Scraper.

---
+++ TalisScrapeWPF/Pages/Home.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TalisScrapeWPF.Pages
+{
+    /// <summary>
+    /// Interaction logic for Home.xaml
+    /// </summary>
+    public partial class Home : UserControl
+    {
+        public Home()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnDoScrape_OnClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnCancelScrape_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
